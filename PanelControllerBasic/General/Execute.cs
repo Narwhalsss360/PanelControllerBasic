be2f6fcc55e6228e629ec977e2d4ffbf45e817b7
Franklyn Vasquez <franklynvasquez24@gmail.com>
2024-05-04 16:09:22 -0400

    Added Execut and AssemblyInfo

---
+++ PanelControllerBasic/General/Execute.cs
@@ -0,0 +1,39 @@
+﻿using PanelController.PanelObjects;
+using PanelController.PanelObjects.Properties;
+using System.Diagnostics;
+
+namespace PanelControllerBasic.General
+{
+    public class Execute : IPanelAction
+    {
+        [UserProperty]
+        public string Path { get; set; } = string.Empty;
+
+        private FileInfo ProcessFile { get => new FileInfo(Path); }
+
+        private Process? _process = null;
+
+        [UserProperty]
+        public bool IsRunning { get => _process is null ? false : !_process.HasExited; }
+
+        public Execute() { }
+
+        public Execute(string path)
+        {
+            Path = path;
+        }
+
+        public object? Run()
+        {
+            if (!ProcessFile.Exists)
+                return $"No file found with path {Path}.";
+            if (_process is not null)
+                return "Process is already running.";
+            if (ProcessFile.Extension.ToLower() != ".exe")
+                return "File type not supported";
+            _process = Process.Start(Path);
+            _process.Exited += (sender, args) => { _process = null; };
+            return null;
+        }
+    }
+}
