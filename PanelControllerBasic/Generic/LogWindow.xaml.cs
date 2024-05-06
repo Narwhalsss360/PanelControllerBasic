a9bbe2208633a6b9dbf744a44750e426d4e40b2c
Franklyn Vasquez <franklynvasquez24@gmail.com>
2024-05-05 23:27:20 -0400

    Added LogWindow

---
+++ PanelControllerBasic/Generic/LogWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using PanelController.Controller;
+using PanelController.PanelObjects;
+using PanelController.PanelObjects.Properties;
+using System.Windows;
+
+namespace PanelControllerBasic.Generic
+{
+    public partial class LogWindow : Window, IPanelObject
+    {
+        private string _format = "/T [/L][/F] /M\n";
+
+        [UserProperty]
+        public string Format
+        {
+            get => _format;
+            set
+            {
+                _format = value;
+                if (!_format.EndsWith('\n'))
+                    _format += '\n';
+            }
+        }
+
+        public LogWindow()
+        {
+            InitializeComponent();
+            Loaded += LogWindow_Loaded;
+            Show();
+        }
+
+        public LogWindow(string format)
+        {
+            InitializeComponent();
+            Loaded += LogWindow_Loaded;
+            Format = format;
+            Show();
+        }
+
+        private void LogWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (Logger.HistoricalLog log in Logger.Logs)
+                AddLog(log);
+            Logger.Logged += (sender, log) => Dispatcher.Invoke(() =>  AddLog(log));
+        }
+
+        private void AddLog(Logger.HistoricalLog log) => LogBox.Text += log.ToString(Format);
+    }
+}
