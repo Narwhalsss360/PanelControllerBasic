e1f7a83b6c1a3fe182705e500947e7c664e3a4f2
Franklyn Vasquez <franklynvasquez24@gmail.com>
2024-06-01 20:18:20 -0400

    Added Auto-Scroll and Format editting to LogWindow

--- PanelControllerBasic/Generic/LogWindow.xaml.cs
+++ PanelControllerBasic/Generic/LogWindow.xaml.cs
@@ -18,6 +18,7 @@ namespace PanelControllerBasic.Generic
                 _format = value;
                 if (!_format.EndsWith('\n'))
                     _format += '\n';
+                FormatTextBox.Text = _format.Substring(0, _format.Length - 1);
             }
         }
 
@@ -41,8 +42,19 @@ namespace PanelControllerBasic.Generic
             foreach (Logger.HistoricalLog log in Logger.Logs)
                 AddLog(log);
             Logger.Logged += (sender, log) => Dispatcher.Invoke(() =>  AddLog(log));
+            FormatTextBox.Text = _format.Substring(0, _format.Length - 1);
         }
 
-        private void AddLog(Logger.HistoricalLog log) => LogBox.Text += log.ToString(Format);
+        private void AddLog(Logger.HistoricalLog log)
+        {
+            LogBox.Text += log.ToString(Format);
+            if (ScrollToEnd.IsChecked ?? false)
+            {
+                LogBox.CaretIndex = LogBox.Text.Length;
+                LogBox.ScrollToEnd();
+            }
+        }
+
+        private void ApplyFormatButton_Click(object sender, RoutedEventArgs e) => Format = FormatTextBox.Text;
     }
 }
